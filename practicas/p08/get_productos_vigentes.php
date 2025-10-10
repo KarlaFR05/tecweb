d8c64e7e3352bd6552307e61460cf957696a680b
KarlaFR05 <karladfr.15@gmail.com>
2025-10-09 23:27:54 -0600

    Ejercicio 6 y 7

--- practicas/p08/get_productos_vigentes.php
+++ practicas/p08/get_productos_vigentes.php
@@ -1,100 +1,66 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
 "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="es">
-<?php
-    //header("Content-Type: application/json; charset=utf-8"); 
-    $data = array();
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <title>Productos Vigentes</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+<body>
+    <h3>PRODUCTOS VIGENTES</h3><br/>
 
-	if(isset($_GET['tope']))
-    {
-		$tope = $_GET['tope'];
+    <?php
+    if (!isset($_GET['tope']) || !is_numeric($_GET['tope'])) {
+        die('Parámetro "tope" no detectado o no válido.');
     }
-    else
-    {
-        die('Parámetro "tope" no detectado...');
+    $tope = (int)$_GET['tope'];
+
+    $link = new mysqli('localhost', 'root', 'Molly23', 'marketzone');
+    if ($link->connect_errno) {
+        die('Falló la conexión: ' . $link->connect_error);
     }
+    $link->set_charset("utf8mb4");
 
-	if (!empty($tope))
-	{
-		/** SE CREA EL OBJETO DE CONEXION */
-		@$link = new mysqli('localhost', 'root', 'Molly23', 'marketzone');
-        /** NOTA: con @ se suprime el Warning para gestionar el error por medio de código */
+    $stmt = $link->prepare("SELECT * FROM productos WHERE unidades <= ? AND eliminado = 0");
+    $stmt->bind_param("i", $tope);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $row = $result->fetch_all(MYSQLI_ASSOC);
+    $stmt->close();
+    $link->close();
 
-		/** comprobar la conexión */
-		if ($link->connect_errno) 
-		{
-			die('Falló la conexión: '.$link->connect_error.'<br/>');
-			//exit();
-		}
+    if (!empty($row)) {
+        echo '<table class="table">
+                <thead class="thead-dark">
+                    <tr>
+                        <th scope="col">#</th>
+                        <th>Nombre</th>
+                        <th>Marca</th>
+                        <th>Modelo</th>
+                        <th>Precio</th>
+                        <th>Unidades</th>
+                        <th>Detalles</th>
+                        <th>Imagen</th>
+                    </tr>
+                </thead>
+                <tbody>';
 
-		/** Crear una tabla que no devuelve un conjunto de resultados */
-		if ( $result = $link->query("SELECT * FROM productos WHERE unidades <= $tope") ) 
-		{
-            /** Se extraen las tuplas obtenidas de la consulta */
-			$row = $result->fetch_all(MYSQLI_ASSOC);
-
-            /** Se crea un arreglo con la estructura deseada */
-            foreach($row as $num => $registro) {            // Se recorren tuplas
-                foreach($registro as $key => $value) {      // Se recorren campos
-                    $data[$num][$key] = utf8_encode($value);
-                }
-            }
-
-			/** útil para liberar memoria asociada a un resultado con demasiada información */
-			$result->free();
-		}
-
-		$link->close();
-
-        /** Se devuelven los datos en formato JSON */
-        //echo json_encode($data, JSON_PRETTY_PRINT);
-	}
-	?>
-	<head>
-		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-		<title>Producto</title>
-		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-	</head>
-	<body>
-		<h3>PRODUCTO</h3>
-
-		<br/>
-		
-		<?php if( isset($row) ) : ?>
-			<table class="table">
-				<thead class="thead-dark">
-					<tr>
-					<th scope="col">#</th>
-					<th scope="col">Nombre</th>
-					<th scope="col">Marca</th>
-					<th scope="col">Modelo</th>
-					<th scope="col">Precio</th>
-					<th scope="col">Unidades</th>
-					<th scope="col">Detalles</th>
-					<th scope="col">Imagen</th>
-					</tr>
-				</thead>
-				<tbody>
-					<?php foreach($row as $value) : ?>
-					<tr>
-						<th scope="row"><?= $value['id'] ?></th>
-						<td><?= $value['nombre'] ?></td>
-						<td><?= $value['marca'] ?></td>
-						<td><?= $value['modelo'] ?></td>
-						<td><?= $value['precio'] ?></td>
-						<td><?= $value['unidades'] ?></td>
-						<td><?= $value['detalles'] ?></td>
-						<td><img src=<?= $value['imagen'] ?> ></td>
-					</tr>
-					<?php endforeach; ?>
-				</tbody>
-			</table>
-		<?php elseif(!empty($id)) : ?>
-
-			 <script>
-                alert('El ID del producto no existe');
-             </script>
-
-		<?php endif; ?>
-	</body>
+        foreach ($row as $value) {
+            echo '<tr>
+                    <th scope="row">' . htmlspecialchars($value['id']) . '</th>
+                    <td>' . htmlspecialchars($value['nombre']) . '</td>
+                    <td>' . htmlspecialchars($value['marca']) . '</td>
+                    <td>' . htmlspecialchars($value['modelo']) . '</td>
+                    <td>$' . number_format($value['precio'], 2) . '</td>
+                    <td>' . $value['unidades'] . '</td>
+                    <td>' . htmlspecialchars($value['detalles']) . '</td>
+                    <td><img src="' . htmlspecialchars($value['imagen']) . '" width="50"></td>
+                  </tr>';
+        }
+        echo '</tbody></table>';
+    } else {
+        echo '<p class="alert alert-warning">No hay productos vigentes con ' . $tope . ' o menos unidades.</p>';
+    }
+    ?>
+</body>
 </html>
