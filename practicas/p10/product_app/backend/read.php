b56f3300653c2e538277f6caf368a1a2bf81db46
KarlaFR05 <karladfr.15@gmail.com>
2025-10-15 20:37:43 -0600

    Ejercicio 2 archivo read.php modificado

--- practicas/p10/product_app/backend/read.php
+++ practicas/p10/product_app/backend/read.php
@@ -3,19 +3,22 @@
 
     // SE CREA EL ARREGLO QUE SE VA A DEVOLVER EN FORMA DE JSON
     $data = array();
-    // SE VERIFICA HABER RECIBIDO EL ID
-    if( isset($_POST['id']) ) {
-        $id = $_POST['id'];
-        // SE REALIZA LA QUERY DE BÚSQUEDA Y AL MISMO TIEMPO SE VALIDA SI HUBO RESULTADOS
-        if ( $result = $conexion->query("SELECT * FROM productos WHERE id = '{$id}'") ) {
-            // SE OBTIENEN LOS RESULTADOS
-			$row = $result->fetch_array(MYSQLI_ASSOC);
+    // SE VERIFICA HABER RECIBIDO ALGÚN PARÁMETRO
+    if (isset($_POST['id']) || isset($_POST['nombre']) || isset($_POST['marca']) || isset($_POST['detalles'])) {
+        // Obtener los valores de los parámetros y agregar el comodín "%" para el LIKE
+        $id = isset($_POST['id']) ? $conexion->real_escape_string($_POST['id']) : '';
+        $nombre = isset($_POST['nombre']) ? "%" . $conexion->real_escape_string($_POST['nombre']) . "%" : '';
+        $marca = isset($_POST['marca']) ? "%" . $conexion->real_escape_string($_POST['marca']) . "%" : '';
+        $detalles = isset($_POST['detalles']) ? "%" . $conexion->real_escape_string($_POST['detalles']) . "%" : '';
 
-            if(!is_null($row)) {
-                // SE CODIFICAN A UTF-8 LOS DATOS Y SE MAPEAN AL ARREGLO DE RESPUESTA
-                foreach($row as $key => $value) {
-                    $data[$key] = $value; // utf8_encode($value);
-                }
+        // Construcción de la consulta de búsqueda
+        $query = "SELECT * FROM productos WHERE id='$id' or nombre LIKE '$nombre' or marca LIKE '$marca' or detalles LIKE '$detalles'";
+        // Ejecutar la consulta
+        if ($result = $conexion->query($query)) {
+            // Verificamos si hay resultados
+            while ($row = $result->fetch_assoc()) {
+                // Agregar cada producto al arreglo de respuesta
+                $data[] = $row;
             }
 			$result->free();
 		} else {
