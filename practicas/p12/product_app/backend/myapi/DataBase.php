1ec38a3db105ccc71c913b34ebd3366f3b340a9a
KarlaFR05 <karladfr.15@gmail.com>
2025-11-14 21:31:47 -0600

    Read products

--- practicas/p12/product_app/backend/myapi/DataBase.php
+++ practicas/p12/product_app/backend/myapi/DataBase.php
@@ -3,24 +3,9 @@ namespace TECWEB\MYAPI;
 
 abstract class DataBase {
     protected $conexion;
-
-    public function __construct($db, $user, $pass) {
-        $this->conexion = @mysqli_connect(
-            'localhost',
-            $user,
-            $pass,
-            $db
-        );
-    
-        /**
-         * NOTA: si la conexión falló $conexion contendrá false
-         **/
-        if(!$this->conexion) {
-            die('¡Base de datos NO conextada!');
-        }
-        /*else {
-            echo 'Base de datos encontrada';
-        }*/
+    public function __construct($db, $user = 'root', $pass = 'Molly23') {
+        $this->conexion = @mysqli_connect('localhost', $user, $pass, $db);
+        if (!$this->conexion) die('¡Base de datos NO conectada!');
     }
 }
 ?>
