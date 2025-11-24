eb5e23668e326b4844dfb3b0ee7aedf0decaf539
KarlaFR05 <karladfr.15@gmail.com>
2025-11-23 23:07:24 -0600

    Paso de parámetros

--- ejemplos/pruebaslim/index.php
+++ ejemplos/pruebaslim/index.php
@@ -6,6 +6,12 @@ $app->get('/', function ( $request, $response, $args ) {
     $response->write("Hola Mundo Slim!!!");
     return $response;
 });
+$app->get("/hola[/{nombre}]", function ($request, $response, $args){
+    $response->write("Hola, " . $args["nombre"]);
+    return $response;
+});
 
 $app->run();
+
+
 ?>
